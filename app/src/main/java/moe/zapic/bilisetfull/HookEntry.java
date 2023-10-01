61b537d6227330a14893df7f278f3d9099c9684c
KawaiiZapic <577358285@qq.com>
2023-10-01 23:20:44 +0800

    fix something

--- app/src/main/java/moe/zapic/bilisetfull/HookEntry.java
+++ app/src/main/java/moe/zapic/bilisetfull/HookEntry.java
@@ -63,6 +63,7 @@ public class HookEntry implements IXposedHookLoadPackage {
                         | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                         | View.SYSTEM_UI_FLAG_LOW_PROFILE
                         | View.SYSTEM_UI_FLAG_IMMERSIVE
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
         );
     }
 }
